60f7c094be3ad846312b08c9fd69de1537f54ef7
Jenya Zueva <jzueva@gmail.com>
2012-11-28 09:27:00 -0500

    different approach

--- brainpickings-scraper.rb
+++ brainpickings-scraper.rb
@@ -20,19 +20,20 @@ SQL
 
 books_url = "http://bookpickings.brainpickings.org/"
 doc = Nokogiri::HTML(open(books_url))
-books_info = doc.css("ol#posts")
-n = 0
-books_info.map do |book|
-  n = n
-  link = doc.css("li.post a").map {|i| i['href']}[n]
-  img = doc.css("li.post img").map {|i| i['src']}[n]
-  title = doc.css("a.post_title h1").map {|title| title.text.strip}[n]
-  author = doc.css("a.post_title h2").map {|title| title.text.strip}[n]
-  description = doc.css("a.post_title p:last-child").map {|title| title.text.strip}[n]
+books_links = doc.css("ol#posts li.post a")
+# .map {|i| i['href']}
 
-db.execute("INSERT INTO books (title, link, author, description, img) 
-            VALUES (?, ?, ?, ?, ?)", title[n], link, author, description, img)
-n += 1
+books_links.map do |book_link|
+  link = book_link['href']
+  doc2 = Nokogiri::HTML(open(link))
+    # link = doc.css("li.post a").map {|i| i['href']}
+    # img = doc.css("li.post img").map {|i| i['src']}
+    puts title = doc.css("ol#posts li.post h1").map {|title| title.text.strip}
+    # author = doc.css("a.post_title h2").map {|title| title.text.strip}
+    # description = doc.css("a.post_title p:last-child").map {|title| title.text.strip}
+
+    # db.execute("INSERT INTO books (title, link, author, description, img) 
+    #             VALUES (?, ?, ?, ?, ?)", title, link, author, description, img)
 
 end
 
